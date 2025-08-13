98b855e527aaf26ce62e79e1bb6549a0e6a01cfe
Carlos <carlos.ruz.b@ug.uchile.cl>
2025-08-12 22:27:12 -0400

    first commit

---
+++ src/lab1.ts
@@ -0,0 +1,50 @@
+/**
+ * =============================
+ *  PREGUNTA 1
+ * =============================
+ */
+
+
+/**
+ * =============================
+ *  PREGUNTA 2
+ * =============================
+ */
+
+const data = require('../data.json') as Bakemon[]
+
+interface Bakemon {
+  // ...
+};
+
+/**
+ * =============================
+ *  PREGUNTA 3
+ * =============================
+ */
+
+function filterByType(bakemons: Bakemon[], type: unknown): unknown {
+  return
+}
+
+/**
+ * =============================
+ *  PREGUNTA 4
+ * =============================
+ */
+
+function addBST(bakemons: Bakemon[]): unknown {
+  return
+}
+
+/**
+ * =============================
+ *  PREGUNTA 5
+ * =============================
+ */
+
+function findStrongestByType(
+  bakemons: Bakemon[]
+): unknown {
+  return
+}
