8ddddbdd7ac456c0dd1b497ad128c512104f4fd1
Zamaya Toledo <zamy.canela@gmail.com>
2025-08-13 21:30:09 -0400

    P4 and P5 complete code

--- src/lab1.ts
+++ src/lab1.ts
@@ -38,27 +38,27 @@
 const data = require('../bakemons.json') as Bakemon[]
 
 interface Bakemon {
-  id: Number,
+  id: number,
   name: string,
   type: string,
-  secondary_type: string,
+  secondary_type: string | undefined,
   stats: {
-    hp: Number,
-    atk: Number,
-    def: Number,
-    sp_atk: Number,
-    sp_def: Number
+    hp: number,
+    atk: number,
+    def: number,
+    sp_atk: number,
+    sp_def: number;
   },
   moves: [
     {
       name: string,
       type: string,
       description: string,
-      power: Number,
-      pp: Number,
-      priority: Number
+      power: number,
+      pp: number,
+      priority: number;
     }
-  ]
+  ];
 };
 
 /**
@@ -67,15 +67,15 @@ interface Bakemon {
  * =============================
  */
 
-function filterByType(bakemons: Bakemon[], type: unknown): unknown {
+function filterByType(bakemons: Bakemon[], type: string): Array<Bakemon> {
   // Para esta función vamos a recorrer los bakemons recibidos y verificamos
   // si el tipo dado coincide con su tipo primario o secundario
 
   const filteredBakemon: Array<Bakemon> = [];
 
   bakemons.forEach((b) => {
-    if (b.type === type as string || b.secondary_type === type as string){
-      filteredBakemon.push(b)
+    if (b.type === type.toUpperCase() || (b.secondary_type !== undefined && b.secondary_type === type.toUpperCase())){
+      filteredBakemon.push(b);
     }
   })
 
@@ -88,8 +88,23 @@ function filterByType(bakemons: Bakemon[], type: unknown): unknown {
  * =============================
  */
 
-function addBST(bakemons: Bakemon[]): unknown {
-  return
+function addBST(bakemons: Bakemon[]): Array<Bakemon & {bst: number}> {
+  // Vamos a recorrer los bakemons, calcular el bst y crear
+  // su nueva representación agregando ese atributo
+
+  const newBakemons: Array<Bakemon & {bst: number}> = [];
+
+  bakemons.forEach((b) => {
+    let bst: number = 0;
+    Object.values(b.stats).forEach((s) => {
+      bst += s;
+    })
+
+    const newB: Bakemon & {bst: number} = {...b, bst};
+    newBakemons.push(newB);
+  })
+
+  return newBakemons;
 }
 
 /**
@@ -98,8 +113,27 @@ function addBST(bakemons: Bakemon[]): unknown {
  * =============================
  */
 
-function findStrongestByType(
-  bakemons: Bakemon[]
-): unknown {
-  return
+function findStrongestByType(bakemons: Bakemon[]): Map<string, Bakemon & {bst: number}> {
+
+  const strongestByType: Map<string, Bakemon & {bst: number}> = new Map();
+  const bakemonsBst: Array<Bakemon & {bst: number}> = addBST(bakemons);
+
+  bakemonsBst.forEach((b) =>{
+    const strongestPrimary: Bakemon & {bst: number} | undefined = strongestByType.get(b.type);
+    if (strongestPrimary === undefined){
+      strongestByType.set(b.type, b);
+    } else if (strongestPrimary.bst < b.bst){
+      strongestByType.set(b.type, b);
+    }
+
+    if (b.secondary_type !== undefined){
+      const strongestSecondary: Bakemon & {bst: number} | undefined = strongestByType.get(b.secondary_type);
+      if (strongestSecondary === undefined){
+        strongestByType.set(b.secondary_type, b);
+      } else if (strongestSecondary.bst < b.bst){
+        strongestByType.set(b.secondary_type, b);
+      }
+    }
+  })
+  return strongestByType;
 }
